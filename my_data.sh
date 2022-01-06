5fc45d7a39310a919330bf61a7c9a173a1d2b676
YiBri1 <YiBri1>
2022-01-06 18:41:43 +0000

    Okay, okay..

--- my_data.sh
+++ my_data.sh
@@ -6,7 +6,7 @@ yum -y install httpd
 myip=`curl http://172.31.46.65/latest/meta-data/local-ipv4`
 
 cat <<EOF > /var/www/html/index.html
-<html>
+<html> myip
 <body bgcolor="grey">
 <h2><font color="gold">Build by Power of Terraform <font color="red"> v0.12</font></h2><br><p>
 <font color="green">Server PrivateIP: <font color="aqua">$myip<br><br>
